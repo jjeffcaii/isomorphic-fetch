a853d77965fd7db2baa97d7982d1832153ff3815
Jeff.Tsai <wcai@maxleap.com>
2016-09-14 15:23:40 +0800

    switch fetch-bower.js to amd

--- fetch-bower.js
+++ fetch-bower.js
@@ -1,1 +1,12 @@
-module.exports = require('fetch');
+(function (factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(["require", "exports", "fetch"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    require('fetch');
+    return fetch;
+});
